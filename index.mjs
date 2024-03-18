ec2ff4b296ead40203d5d28667010bf6b8977db5
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-03-18 06:52:54 +0500

    CORS Policy

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import router from "./routes/index.mjs";
 import db from './config/db.mjs'
 import { PORT } from "./config/environment.mjs"
@@ -8,6 +9,10 @@ const app = express()
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 })
+app.use(cors({
+    origin: `http://localhost:${PORT}`, // Allow requests from localhost:3000
+    credentials: true // Enable sending cookies with CORS requests
+}));
 app.use(express.json())
 
 app.use('/', router)
